62336a80689630c25786a13a90f81d6e2512cac6
Valeriapino3 <pinodeornelas.valeria@gmail.com>
2024-09-13 00:34:36 -0400

    3

--- CVapp/views.py
+++ CVapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the CVapp index.")
+    return HttpResponse("Welcome to my CV page.")
